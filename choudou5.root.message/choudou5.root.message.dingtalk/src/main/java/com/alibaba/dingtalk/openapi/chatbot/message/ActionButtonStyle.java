42a4cbac2512495b9239d98b06135c645648dbff
xuhaowende <xuhaowende@sina.cn>
2018-02-06 22:34:53 +0800

    update package

--- choudou5.root.message/choudou5.root.message.dingtalk/src/main/java/com/alibaba/dingtalk/openapi/chatbot/message/ActionButtonStyle.java
+++
@@ -1,9 +0,0 @@
-package com.alibaba.dingtalk.openapi.chatbot.message;
-
-/**
- * Created by dustin on 2017/3/19.
- */
-public enum ActionButtonStyle {
-    HORIZONTAL,
-    VERTICAL
-}
--- choudou5.root.message/choudou5.root.message.dingtalk/src/main/java/com/alibaba/dingtalk/openapi/message/Message.java
+++
@@ -1,8 +0,0 @@
-package com.alibaba.dingtalk.openapi.message;
-
-
-import java.io.Serializable;
-
-public abstract class Message implements Serializable{
-	public abstract String type();
-}
