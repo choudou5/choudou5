078813f80593d59a108bce915f791ad7ce024049
xuhaowende <xuhaowende@sina.cn>
2018-03-14 01:10:24 +0800

    update

--- choudou5.rpc/choudou5.rpc.dubbo/src/main/java/com/choudou5/rpc/dubbo/service/impl/AbstractService.java
+++ choudou5.rpc/choudou5.rpc.dubbo/src/main/java/com/choudou5/rpc/dubbo/service/impl/AbstractService.java
@@ -14,19 +14,12 @@
  */
 package com.choudou5.rpc.dubbo.service.impl;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-
-import com.alibaba.dubbo.config.ApplicationConfig;
-import com.alibaba.dubbo.config.ReferenceConfig;
-import com.alibaba.dubbo.config.RegistryConfig;
-import com.choudou5.rpc.dubbo.service.RemoteLogService;
+import com.alibaba.dubbo.common.URL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.alibaba.dubbo.common.URL;
-import com.alibaba.dubbo.registry.RegistryService;
+import java.util.Map;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * IbatisDAO
@@ -48,78 +41,6 @@ public class AbstractService {
 
     public ConcurrentMap<String, ConcurrentMap<String, Map<Long, URL>>> getRegistryCache(){
         return sync.getRegistryCache();
-    }
-
-
-
-    // 注册中心信息缓存
-    private static Map<String, RegistryConfig> registryConfigCache = new ConcurrentHashMap<>();
-    private static Map<String, ReferenceConfig<RemoteLogService>> logCache = new ConcurrentHashMap<>();
-    private static ApplicationConfig application = new ApplicationConfig();
-    static {
-        application.setName("test");
-    }
-
-
-    /**
-     * 获取注册中心信息
-     *
-     * @param address
-     *            zk注册地址
-     * @param group
-     *            dubbo服务所在的组
-     * @return
-     */
-    private static RegistryConfig getRegistryConfig(String address, String group, String version) {
-        String key = address + "-" + group + "-" + version;
-        RegistryConfig registryConfig = registryConfigCache.get(key);
-        if (null == registryConfig) {
-            registryConfig = new RegistryConfig();
-            registryConfig.setAddress(address);
-            registryConfig.setGroup(group);
-
-            registryConfigCache.put(key, registryConfig);
-        }
-        return registryConfig;
-    }
-
-
-    /**
-     * 获取服务的代理对象
-     * @param business
-     * @param address
-     * @param group
-     * @return
-     */
-    private static ReferenceConfig<RemoteLogService> getReferenceConfig(String business, String address, String group, String version) {
-        String referenceKey = business+address;
-        ReferenceConfig<RemoteLogService> referenceConfig = logCache.get(referenceKey);
-        if (null == referenceConfig) {
-            referenceConfig = new ReferenceConfig<>();
-            referenceConfig.setApplication(application);
-            referenceConfig.setRegistry(getRegistryConfig(address, group, version));
-            referenceConfig.setInterface(RemoteLogService.class);
-            referenceConfig.setVersion(version);
-            logCache.put(referenceKey, referenceConfig);
-        }
-        return referenceConfig;
-    }
-
-
-    /**
-     * 调用远程服务
-     * @param business
-     * @return
-     */
-    public static boolean invoke(String business, String address, String group, String version) {
-        ReferenceConfig<RemoteLogService> reference = getReferenceConfig(business, address, group, version);
-        if (null != reference) {
-            RemoteLogService callbackService = reference.get();
-            if (null != callbackService) {
-                callbackService.findList(1);
-            }
-        }
-        return false;
     }
 
 }
--- choudou5.rpc/choudou5.rpc.dubbo/src/test/java/com/choudou5/rpc/dubbo/service/ProviderServiceTest.java
+++ choudou5.rpc/choudou5.rpc.dubbo/src/test/java/com/choudou5/rpc/dubbo/service/ProviderServiceTest.java
@@ -1,9 +1,11 @@
 package com.choudou5.rpc.dubbo.service;
 
+import com.alibaba.dubbo.config.spring.ReferenceBean;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 
-import java.util.Date;
+import java.util.List;
 
 /**
  * @Name：ProviderServiceTest 说明
@@ -21,5 +23,30 @@ public class ProviderServiceTest extends BaseTest{
     public void findAddresses(){
 
         print(providerService.findAll());
+    }
+
+    @Test
+    public void getService(){
+        RemoteLogService service = getService(null, "123", 123, RemoteLogService.class);
+        service.findList(10);
+    }
+
+    private RemoteLogService getService(ApplicationContext applicationContext, String host, int port, Class serviceClass) {
+        String url = "dubbo://"+host+":"+port+"/"+serviceClass.getName();//更改不同的Dubbo服务暴露的ip地址&端口
+        ReferenceBean<RemoteLogService> referenceBean = new ReferenceBean<RemoteLogService>();
+        referenceBean.setApplicationContext(applicationContext);
+        referenceBean.setInterface(serviceClass);
+        referenceBean.setUrl(url);
+        try {
+            referenceBean.afterPropertiesSet();
+            return referenceBean.get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    interface RemoteLogService {
+        List<String> findList(int top);
     }
 }
